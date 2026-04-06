6767faad7ebd062f01b3c2e5eea5795fdc6f2c68
Warrior07a <akshatmittalofficial@gmail.com>
2026-04-07 03:06:14 +0530

    feat : ws room logic done

--- Backend/routes/index.ts
+++ Backend/routes/index.ts
@@ -6,12 +6,13 @@ import express, {
 import { prisma } from "../prisma/db";
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import { SignupSchema, SigninSchema } from "../Types/types";
+import cors from "cors";
 
 const app = express();
 const SECRET = process.env.JWT_SECRET ?? "changeme_in_prod";
 
 app.use(express.json());
-
+app.use(cors());
 // ── helpers ──────────────────────────────────────────────────────────────────
 function ferr(msg: string, code: number, res: Response) {
   return res.status(code).json({ success: false, data: null, error: msg });
@@ -20,25 +21,29 @@ function ferr(msg: string, code: number, res: Response) {
 
 export function authMiddleware() {
   return (req: Request, res: Response, next: NextFunction) => {
+    // console.log(req.headers);
+    // console.log(req.headers.authorization);
     try {
       const token = req.headers.authorization?.split(" ")[1];
+      // console.log(token);
       if (!token) return ferr("UNAUTHORIZED", 401, res);
       const payload = jwt.verify(token, SECRET) as JwtPayload;
       req.id = payload.id;
       req.role = payload.role;
       next();
-    } catch {
+    } catch (e){
+      console.log(e.message);
       return ferr("UNAUTHORIZED", 401, res);
     }
   };
 }
 
 // ── routes ───────────────────────────────────────────────────────────────────
-app.post("api/auth/signup", async (req: Request, res: Response) => {
+app.post("/api/auth/signup", async (req: Request, res: Response) => {
   const parsed = SignupSchema.safeParse(req.body);
   if (!parsed.success) return ferr("INVALID_REQUEST", 400, res);
 
-  const exists = await prisma.users.findUnique({
+  const exists = await prisma.users.findFirst({
     where: { email: parsed.data.email },
   });
   if (exists) return ferr("EMAIL_ALREADY_EXISTS", 400, res);
@@ -70,9 +75,8 @@ app.post("/api/auth/login", async (req: Request, res: Response) => {
 
 
 
-app.post("/teacher/createroom", 
-  // authMiddleware(),
-   async (req: Request, res: Response) => {
+app.post("/teacher/createroom", authMiddleware(), async (req: Request, res: Response) => {
+  console.log("reached  cretaed room here");
   let id = req.id
   let role = req.role
   if (role != "teacher") {
@@ -81,6 +85,7 @@ app.post("/teacher/createroom",
   if (!id || !role) {
     return ferr("ID OR ROLE MISSING", 404, res);
   }
+  console.log("making db call fron backend");
   const roomadd = await prisma.rooms.create({
     data: {
       teacher_id: id,
@@ -90,46 +95,60 @@ app.post("/teacher/createroom",
     return res.status(201).json({
       status: " room has been crrated successfully",
       success: true,
-      roomid: roomadd.id
+      roomid: roomadd.id,
+      teacher_id : roomadd.teacher_id
     })
+    console.log(roomadd);
   }
 })
 
-app.put("/user/join",
-  //  authMiddleware(),
-    async (req: Request, res: Response) => {
-  const roomId = req.query.roomId as string;
-  if (!roomId) {
-    return ferr("ROOMID MISSING", 403, res);
-  }
-  let id = req.id;
-  let role = req.role;
-  if (!id || !role) {
-    return ferr("ID OR ROLE MISSING", 404, res);
-  }
-  const roomfind = await prisma.rooms.update({
-    where: {
-      id: roomId
-    },
-    data: {
-      student_id: id
+app.put("/user/join", authMiddleware(), async (req: Request, res: Response) => {
+    const url = req.url
+    console.log(url);
+    const roomId = req.query.roomId as string;
+    if (!roomId) {
+      return ferr("ROOMID MISSING", 403, res);
     }
+    let id = req.id;
+    let role = req.role;
+    if (!id || !role) {
+      return ferr("ID OR ROLE MISSING", 404, res);
+    }
+    const isRoomFull = await prisma.rooms.findFirst({
+      where: {
+        id: roomId
+      }
+    })
+
+    if (isRoomFull?.student_id == id || isRoomFull?.teacher_id == id){
+      return res.status(200).json({
+        msg : "WELCOME BACK ",
+        student_id : isRoomFull.student_id
+      })
+    }
+    if (isRoomFull?.student_id && isRoomFull.student_id) {
+      return ferr("Room is already Full", 401, res);
+    }
+    const roomfind = await prisma.rooms.update({
+      where: {
+        id: roomId
+      },
+      data: {
+        student_id: id
+      }
+    })
+    if (!roomfind) {
+      return ferr("no such room exists", 404, res);
+    }
+    return res.status(201).json({
+      msg: "record has been updated successfully",
+      success: true,
+      student_id : roomfind.student_id  
+    })
   })
-  if (!roomfind) {
-    return ferr("no such room exists", 404, res);
-  }
-  return res.status(201).json({
-    msg: "record has been updated successfully",
-    success: true
-  })
-})
-
-
-
-
 
 
 // ── start ────────────────────────────────────────────────────────────────────
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
--- Backend/socket/ws.ts
+++ Backend/socket/ws.ts
@@ -1,9 +1,62 @@
+import type { Request } from "express";
 import { WebSocket, WebSocketServer } from "ws";
-const wss = new WebSocketServer({port : 8080});
+import axios from "axios";
+import url from "url"
 
-let ws_conn = [];
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection" , async(ws)=>{
-    ws_conn.push(ws);
-    ws.on("message" , )
-})
+// type IncomingData = {
+//     type : string,
+//     teacher_id : string,
+//     room_id : string
+// }
+
+type obj = {
+    teacher_id: string,
+    teacher_ws: WebSocket,
+    student_id?: string,
+    student_ws?: WebSocket
+}
+
+let memory: Map<string, obj> = new Map();
+
+wss.on("connection", (ws: WebSocket, req: Request) => {
+    console.log("connection made");
+    ws.on("message", (data: any) => {
+        console.log("message received");
+        data = JSON.parse(data);
+        console.log(data);
+        if (data.type == "CREATE_ROOM") {
+            let obj = {
+                teacher_id: data.teacher_id,
+                teacher_ws: ws
+            }
+            memory.set(data.room_id, obj);
+            console.log("Room create logic reached");
+            console.log(memory);
+            ws.send(
+                JSON.stringify(
+                    {
+                        type: "ROOM_CREATED",
+                        payload: {}
+                    }
+                )
+            );
+        }
+        else if (data.type == "JOIN_ROOM"){
+            const roomexist = memory.get(data.room_id);
+            if (roomexist){
+                roomexist["student_id"] = data.student_id;
+                roomexist["student_ws"] = ws;
+            }
+            ws.send(
+                JSON.stringify({
+                    type : "updated websockets", 
+                    payload : {}
+                })
+            )
+        }
+    })
+}
+
+)
