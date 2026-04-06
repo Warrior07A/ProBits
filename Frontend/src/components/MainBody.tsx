f97be37b020bca077afb815d4a983eb3f0350dbe
Warrior07a <akshatmittalofficial@gmail.com>
2026-04-07 03:04:46 +0530

    feat:connected FE+BE for websockets

--- Frontend/src/components/MainBody.tsx
+++ Frontend/src/components/MainBody.tsx
@@ -8,6 +8,35 @@ const ws = new WebSocket("ws://localhost:8080");
 export default function MainBody() {
     const editorinput = useRef(""); 
     const [trigger,settrigger] = useState(false);
+    let [roomid ,setroom_id] = useState("");
+    let [JoinRoomId , SetJoinRoomId ]= useState("");
+
+
+
+    async function JoinRoom(){
+        if(JoinRoomId == ""){
+            return alert("Enter a valid room id");
+        }
+        const res = await axios.put("http://localhost:3001/user/join?roomId=" + JoinRoomId ,{} ,{
+            headers : {
+                Authorization : localStorage.getItem("Authorization")
+            }
+        });
+        console.log(res);
+        if (res.status == 201 ){
+            alert("You have joined the Room");
+            ws.send(JSON.stringify({
+                type : "JOIN_ROOM",
+                student_id : res.data.student_id,
+                room_id : JoinRoomId
+            }))
+
+        }
+        else if (res.status == 200){
+            alert("Welcome back");
+        }
+
+    }
 
      useEffect(()=>{
         async function dbcall(){
@@ -29,15 +58,18 @@ export default function MainBody() {
                         console.log(data);
                         if (data.type == "ROOM_CREATED"){
                             alert("room has been created");
+                            setroom_id(roomid => room_id);
+                            settrigger(trigger => false);
                             return;
                         }
-                        else{
-                            
-                        }
+                        console.log("else mein fatta");
+                        return;
                     }
                 }
         }
-        dbcall();
+        if (trigger){
+            dbcall();
+        }
     },[trigger])
 
     return (
@@ -60,7 +92,28 @@ export default function MainBody() {
                         onClick={()=>{settrigger(!trigger) }}
                         className="border border-blue-900 bg-red-700">Create Room 
                     </button>
-                    <button> Join Room</button>
+                    <div> 
+                        {roomid ? <h2> Room Created !</h2> :<h2> Create a Room </h2>}
+                        <div className="flex">
+                            <div>
+                                <h3 className="bg-white border border-e-black w-9/10">{roomid}</h3>
+                                </div>
+                                {
+                                    roomid ? 
+                                <div>
+                                    <button
+                                        onClick={()=>{navigator.clipboard.writeText(roomid) , alert("Copied to clipboard")}}
+                                        className="bg-white"> Copy </button>
+                                </div> : ""
+                                 }
+                        </div>
+                    </div>
+                    <input
+                        className="border border-black bg-amber-50"
+                        type = "text" placeholder="Enter Room Id to Join"
+                        onChange = {(e)=>{SetJoinRoomId (e.target.value) }}
+                        ></input>
+                    <button className="bg-red-600" onClick={()=>{JoinRoom()}}> Join Room</button>
                 </div>
 
             </div>
